ad2f33175c43f474314b1fb9df679ad212aa0400
Kowacka Malgorzata <kowackag@gmail.com>
2022-02-23 13:33:22 +0100

    Add saving data in localStorage

--- src/store.js
+++ src/store.js
@@ -3,6 +3,14 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const preloadedState = JSON.parse(localStorage.getItem('savings')) || undefined;
+
+const store = createStore(reducers,preloadedState, composeWithDevTools(applyMiddleware(thunk)));
+
+
+store.subscribe(()=>{
+    const state = store.getState();
+    localStorage.setItem('savings', JSON.stringify(state))
+})
 
 export default store;
